ce6f6617d510a0f0026838a58c82dac8040917e1
muhammedvuslat <v.sltcevikev@gmail.com>
2022-09-14 20:34:37 +0300

    app.js_created-end

--- -Mini Projects-/01_Program Lang. Lister/app.js
+++ -Mini Projects-/01_Program Lang. Lister/app.js
@@ -1,1 +1,35 @@
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
 
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
+
+deleteBtn.addEventListener("click", () => {
+  newUl.childElementCount > 0
+    ? newUl.removeChild(newUl.lastElementChild)
+    : alert("There is no item to delete");
+});
+
+langInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    addBtn.click();
+  }
+  if (event.keyCode === 46) {
+    deleteBtn.click();
+  }
+});
+
+window.addEventListener("load", () => {
+  langInput.focus();
+});
