565422e2024be675507dfa73dc51df39e3c8b0ef
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-06 18:02:44 +0300

    js-2

---
+++ 06_Weather-Project/app.js
@@ -0,0 +1,10 @@
+function rain() {
+  document.body.style.backgroundColor = "#045b7e";
+  document.body.style.backgroundImage = "url(./img/2rain.gif)";
+  document.body.style.backgroundSize = "40%";
+}
+function snow() {
+  document.body.style.backgroundColor = "#045b7e";
+  document.body.style.backgroundImage = "url(./img/snow.gif)";
+  document.body.style.backgroundSize = "40%";
+}
