2e89ee7a6af49f3ef0d6f5837e19a090e0b3d435
muhammedvuslat <v.sltcevikev@gmail.com>
2022-10-18 21:01:49 +0300

    v.1(started-cards)

---
+++ 1_React Project/01_Tour -Places/tour-places/src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
+
+const Main = () => {
+  return (
+    <div className="card-container">
+      {data.map((item, id) => (
+        <Card {...item} key={id} />
+      ))}
+    </div>
+  );
+};
+export default Main;
