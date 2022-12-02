00cd3e63856eb637f3903c1879ca6688cc340647
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-02 19:56:19 +0300

    v.2.0(Login session-storage)

--- 1_React Project/03_Recipe-App/src/pages/login/Login.jsx
+++ 1_React Project/03_Recipe-App/src/pages/login/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import { useNavigate } from "react-router-dom";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const userInfo = {
+    usename: "Admin",
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
 
-export default Login
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"Welcome"} Recipe Login</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter Username" type="text" />
+          <StyledInput placeholder="Enter Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
