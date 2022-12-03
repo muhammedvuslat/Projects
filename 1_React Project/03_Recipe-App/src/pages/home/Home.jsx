99aee613eb2120362a26d62da0aac5874d01aea7
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-03 11:49:58 +0300

    v.5.0(cards)

--- 1_React Project/03_Recipe-App/src/pages/home/Home.jsx
+++ 1_React Project/03_Recipe-App/src/pages/home/Home.jsx
@@ -1,30 +1,52 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/Header";
+import Cards from "../../components/cards/Cards";
+import "./Home.style";
+import { HeaderText, HomeImg, ImgDiv } from "./Home.style";
+import homeSvg from "../../assets/home.svg";
 
 const Home = () => {
   const APP_ID = "e9ad5c8c";
   const APP_KEY = "359bb19e1143a5bb4679c215903c1cb7";
   const [query, setQuery] = useState("egg");
   const [selectedMeal, setSelectedMeal] = useState("breakfast");
-  const [recipes, setRecipses] = useState("");
+  const [recipes, setRecipses] = useState(null);
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    const { data } = await axios(url);
-
-    setRecipses(data.hits);
+    if (query) {
+      try {
+        const { data } = await axios(url);
+        setRecipses(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Fill the Form");
+    }
   };
   console.log(recipes);
 
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <div>
-      <Header setQuery={setQuery} />
+      <Header
+        setQuery={setQuery}
+        setSelectedMeal={setSelectedMeal}
+        mealType={mealType}
+        getData={getData}
+      />
+      {!recipes && (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+        </ImgDiv>
+      )}
+      {recipes?.length === 0 && (
+        <HeaderText>The Food can not be found</HeaderText>
+      )}
+      {recipes?.length > 0 && <Cards recipes={recipes} />}
     </div>
   );
 };
