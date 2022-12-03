9e16f10d5833f3e57e7da8f55a79970be382f20d
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-03 09:23:38 +0300

    v.4.0(header-search)

--- 1_React Project/03_Recipe-App/src/components/header/Header.jsx
+++ 1_React Project/03_Recipe-App/src/components/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import {
+  Button,
+  FoodInput,
+  FormContainer,
+  HeaderContainer,
+  MainHeader,
+} from "./Header.style";
 
-const Header = () => {
+const Header = (setQuery) => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
 
-export default Header
+      <FormContainer>
+        <FoodInput
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        ></FoodInput>
+        <Button type="text">Search</Button>
+      </FormContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
--- 1_React Project/03_Recipe-App/src/pages/home/Home.jsx
+++ 1_React Project/03_Recipe-App/src/pages/home/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
+  const APP_ID = "e9ad5c8c";
+  const APP_KEY = "359bb19e1143a5bb4679c215903c1cb7";
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipses] = useState("");
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    const { data } = await axios(url);
+
+    setRecipses(data.hits);
+  };
+  console.log(recipes);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      Home
+      <Header setQuery={setQuery} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
