abf49343a40b9fca0a9b59d14ffb3e8d45e8d4a9
muhammedvuslat <v.sltcevikev@gmail.com>
2022-12-03 12:02:35 +0300

    v.5.1(cards-buttons)

--- 1_React Project/03_Recipe-App/src/components/cards/Cards.jsx
+++ 1_React Project/03_Recipe-App/src/components/cards/Cards.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./Cards.style";
-import { Card, Header, MainContainer } from "./Cards.style";
+import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
+import { useNavigate } from "react-router-dom";
+import defaultImage from "../../assets/default-image.jpg";
 
 const Cards = ({ recipes }) => {
+  const navigate = useNavigate();
   return (
     <MainContainer wrap="wrap">
       {recipes.map(({ recipe }, index) => {
         return (
           <Card key={index}>
             <Header>{recipe.label} </Header>
+            <Image src={recipe.image || defaultImage} />
+            <Button
+              onClick={() =>
+                navigate("detail", { state: recipe, replace: false })
+              }
+            >
+              View More
+            </Button>
           </Card>
         );
       })}
