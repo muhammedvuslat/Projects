86d7de331468142913bf7f7decd9faac3f98cbde
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-04 22:20:01 +0300

    views_created(v.2)

--- 4_Django Projects/main/settings.py
+++ 4_Django Projects/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ti4+skpl0br*a)&tn4fx8ufz)+sjkqmqs_t6h3d@1#okq#i+#x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo'
+
 ]
 
 MIDDLEWARE = [
--- 4_Django Projects/todo/models.py
+++ 4_Django Projects/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    PRIORITY = (
+        (1, 'High'), #! 1 back-end , High Front-end
+        (2, 'Medium'),
+        (3, 'Low')
+    )
+    task = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    priority = models.SmallIntegerField(choices=PRIORITY, default=3)
+    is_done = models.BooleanField(default=False)
+    updated_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.task} <{self.get_priority_display()}>'
