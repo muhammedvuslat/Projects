fbd5e491a70179363b11319f7e30e023aab6f436
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-04 22:33:04 +0300

    views_created(CBV)(v.2.1)

--- 4_Django Projects/todo/views.py
+++ 4_Django Projects/todo/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render , get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 #My File İmport
 from .models import Todo
@@ -13,6 +14,8 @@ from .serializers import TodoSerializer
 @api_view()
 def todo_home(request):
     return Response({'Home message':'Welcome Home'})
+
+#! FBV (Function Based Views)
 
 @api_view(['GET','POST'])
 def todo_list_crate(request):
@@ -42,4 +45,13 @@ def todo_detail(request,id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#! CBV (Class Based Views)
+class Todos(ListCreateAPIView):
+    queryset = Todo.objects.filter(is_done = False)
+    serializer_class = TodoSerializer
+
+class TodoDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
