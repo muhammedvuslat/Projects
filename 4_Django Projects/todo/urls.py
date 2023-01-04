e972ee1cea37310d7cb52b06744b41f91b4d4be9
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-04 22:33:04 +0300

    views_created(CBV)(v.2.1)

--- 4_Django Projects/todo/urls.py
+++ 4_Django Projects/todo/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path,include
 
 from .views import (
+    #!FBV
     todo_home,
     todo_list_crate,
-    todo_detail
+    todo_detail,
+    #!CBV
+    Todos,
+    TodoDetail,
 )
 
 
 urlpatterns = [
     path('',todo_home),
     path('list/',todo_list_crate),
-    path('detail/<int:id>',todo_detail)
+    path('detail/<int:id>',todo_detail),
+    path('list_class/',Todos.as_view()),
+    path('detail_class/<int:id>',TodoDetail.as_view()),
 ]
