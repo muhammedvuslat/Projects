62ed471c2c27e5507305e0b93403ae4ac2190cfc
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-10 02:30:46 +0300

    models-urls_created(v.2)

--- 4_Django Projects/02_Blog-App/blog/admin.py
+++ 4_Django Projects/02_Blog-App/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog,Category
+
+admin.site.register(Category)
+admin.site.register(Blog)
+
 
 # Register your models here.
--- 4_Django Projects/02_Blog-App/blog/models.py
+++ 4_Django Projects/02_Blog-App/blog/models.py
@@ -5,3 +5,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Blog(models.Model):
+    Status_Choices = (
+        ("p","Published"),
+        ("d","Draft")
+    )
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    title = models.CharField(unique=True, max_length=25)
+    content = models.TextField()
+    status = models.CharField(choices=Status_Choices,default="d", max_length=20)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} |{self.get_status_display()}|"
