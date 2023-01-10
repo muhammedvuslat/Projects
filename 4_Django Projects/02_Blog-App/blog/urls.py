a4d1eb1d5dc847445e48adee8af13ae8b7693543
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-10 09:38:33 +0300

    serializer-urls_router-views-pgintn-fltr-srch(v.3)

--- 4_Django Projects/02_Blog-App/blog/urls.py
+++ 4_Django Projects/02_Blog-App/blog/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,include
+from .views import CategoryView , BlogView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('category',CategoryView)
+router.register('blog',BlogView)
 
 
 urlpatterns = [
-    path('category/',pass),
-    path('blog/',pass)
+    path('',include(router.urls))
 ]
