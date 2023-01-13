d2739e964084e457739b22781e925b72a55be8b1
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-13 22:40:11 +0300

    swagger-debug_toolbar-urls.py_created(v.1)

--- 4_Django Projects/03_Flight_App/main/settings.py
+++ 4_Django Projects/03_Flight_App/main/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #! My app
+
+    #!Third Party
+    'rest_framework',
+    ##!Swagger
+    'drf_yasg',
+    ##!Debug Toolbar
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'main.urls'
@@ -121,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [ 
+    "127.0.0.1",
+]
