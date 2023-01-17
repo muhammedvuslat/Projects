074a5eb4a2a6d41651958c4524a8e32a5149d1b0
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-17 12:23:37 +0300

    serializer_views_urls_permissons(v.8)

---
+++ 4_Django Projects/03_Flight_App/flight/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from rest_framework import routers
+from .views import FlightView, ReservationView
+
+router = routers.DefaultRouter()
+router.register("flights", FlightView)
+router.register("reservation", ReservationView)
+urlpatterns = [
+    
+]
+
+urlpatterns += router.urls
--- 4_Django Projects/03_Flight_App/flight/views.py
+++ 4_Django Projects/03_Flight_App/flight/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAdminUser
+from .serializers import FlightSerializer, ReservationSerializer
+from .models import Flight , Reservation
+from .permissions import IsStafforReadOnly
 
-# Create your views here.
+class FlightView(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = [IsStafforReadOnly]
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
