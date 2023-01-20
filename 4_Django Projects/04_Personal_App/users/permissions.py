d0746995fb029705ab10b4d9c3590b6fec612878
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-21 02:54:24 +0300

    Personal_app-permissions-bug-fixed(v.3.1)

--- 4_Django Projects/04_Personal_App/users/permissions.py
+++ 4_Django Projects/04_Personal_App/users/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class IsOwnerOrStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(request.user.is_staff or (object.user == request.user))
+        return bool(request.user.is_staff or (obj.user == request.user))
