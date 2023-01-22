0a4b255e40fc81ddb4d366542ec93931cfdee46f
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-23 00:31:41 +0300

    Department_Personnel_serializers-views(v.5)

--- 4_Django Projects/04_Personal_App/personnel/views.py
+++ 4_Django Projects/04_Personal_App/personnel/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .serializers import DeparmentSerializer, PersonnelSerializer
+from rest_framework import generics
+from .models import Department, Personnel
 
-# Create your views here.
+
+class DepartmentView(generics.ListAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DeparmentSerializer
+
+
+class PersonnelView(generics.ListAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class = PersonnelSerializer
