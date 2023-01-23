20a397f88907a72ba73df9e3549f239b321e9dcd
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-23 21:24:04 +0300

    Department_Personnel-Permissions(v.7)

--- 4_Django Projects/04_Personal_App/personnel/serializers.py
+++ 4_Django Projects/04_Personal_App/personnel/serializers.py
@@ -1,16 +1,46 @@
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from .models import Department, Personnel
+from django.utils.timezone import now
 
 
 class DeparmentSerializer(serializers.ModelSerializer):
+    personnel_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Department
-        fields = "__all__"
+        fields = ("id", "name", "personnel_count")
+
+    def get_personnel_count(self, obj):
+        return obj.personnels.count()
 
 
 class PersonnelSerializer(serializers.ModelSerializer):
 
+    days_since_joined = serializers.SerializerMethodField()
+    create_user_id = serializers.IntegerField(required=False)
+    create_user = serializers.StringRelatedField()
+
     class Meta:
         model = Personnel
         fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data["create_user_id"] = self.context['request'].user.id
+        instance = Personnel.objects.create(**validated_data)
+        return instance
+
+    def get_days_since_joined(self, obj):
+        return (now() - obj.start_date).days
+
+
+class DepartmentPersonnelSerializer(serializers.ModelSerializer):
+
+    personnel_count = serializers.SerializerMethodField()
+    personals = PersonnelSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Department
+        fields = ("id", "name", "personnel_count", "personals")
+
+    def get_personnel_count(self, obj):
+        return obj.personals.count()
--- 4_Django Projects/04_Personal_App/personnel/urls.py
+++ 4_Django Projects/04_Personal_App/personnel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DepartmentView, PersonnelListView
+from .views import DepartmentView, PersonnelListView, DepartmentPersonnelView, PersonalGetUpdateDelete
 
 urlpatterns = [
     path("department/", DepartmentView.as_view()),
-    path("personnel/", PersonnelListView.as_view())
+    path("personnel/", PersonnelListView.as_view()),
+    path("personnel/<int:pk>", PersonalGetUpdateDelete.as_view()),
+    path("department/<str:department>", DepartmentPersonnelView.as_view()),
 ]
