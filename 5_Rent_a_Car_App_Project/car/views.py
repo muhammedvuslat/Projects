72fb8068c3cbe0be08ea3af0fcf3de8aab4ecc64
muhammedvuslat <v.sltcevikev@gmail.com>
2023-01-30 10:02:30 +0300

    user_permissons(v.3)

--- 5_Rent_a_Car_App_Project/car/views.py
+++ 5_Rent_a_Car_App_Project/car/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Car , Reservation
+from .serializers import CarSerializer
+from .permissions import IsStaffOrReadOnly
+
+
+class CarView(ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsStaffOrReadOnly,]
+
+
