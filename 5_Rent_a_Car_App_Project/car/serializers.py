708f83587a0d3e1db90d3c75174b3b21888bf268
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-02 01:36:57 +0300

    views_get_fields-methods(v.6)

--- 5_Rent_a_Car_App_Project/car/serializers.py
+++ 5_Rent_a_Car_App_Project/car/serializers.py
@@ -13,3 +13,12 @@ class CarSerializer(serializers.ModelSerializer):
             'rent_per_day',
             'availability'
         )
+    def get_fields(self):
+        fields_all = super().get_fields()
+        request = self.context.get('request')
+
+        if request.user and not request.user.is_staff:
+            fields_all.pop('availability')
+            fields_all.pop('plate_number')
+
+        return fields_all
