4c4322f78c6c3e4ffe2ade14579030006c5d5d67
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-12 00:23:04 +0300

    Models_created(v1)

--- 4_Django Projects/06_Stock_App_Project/stock/admin.py
+++ 4_Django Projects/06_Stock_App_Project/stock/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Brand, Category, Firm, Product, Purchases, Sales
+
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Firm)
+admin.site.register(Product)
+admin.site.register(Purchases)
+admin.site.register(Sales)
 
 # Register your models here.
