15a6e2ec778dc34a35d994936765b5192295a3c4
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-12 10:46:08 +0300

    Serializer&view&urls&created(v.2)

---
+++ 4_Django Projects/06_Stock_App_Project/stock/serializer.py
@@ -0,0 +1,108 @@
+from rest_framework import serializers
+from .models import Brand, Category, Firm, Product, Purchases, Sales
+
+class CategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+            'product_count'            
+        )
+
+    def get_product_count(self,obj):
+        return Product.objects.filter(category_id=obj.id).count()
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = (
+            'id',
+            'name',
+            'image'
+        )
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+    category_id = serializers.IntegerField()
+    brand_id = serializers.IntegerField()
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'category',
+            'category_id',
+            'brand',
+            'brand_id',
+            'stock'            
+        )
+
+class FirmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Firm
+        fields = (
+            'id',
+            'name',
+            'phone',
+            'adress',
+            'image'
+        )
+
+class PurchasesSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    firm = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+    product = serializers.StringRelatedField()
+    user_id = serializers.IntegerField()
+    firm_id = serializers.IntegerField()
+    brand_id = serializers.IntegerField()
+    product_id = serializers.IntegerField()
+
+    class Meta:
+        model = Purchases
+        fields = (
+            'id',
+            'user',
+            'user_id',
+            'firm',
+            'firm_id',
+            'brand',
+            'brand_id',
+            'product', 
+            'product_id', 
+            'quantity',
+            'price',
+            'price_total'
+        )
+
+class SalesSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+    product = serializers.StringRelatedField()
+    user_id = serializers.IntegerField()
+    brand_id = serializers.IntegerField()
+    product_id = serializers.IntegerField()
+
+    class Meta:
+        model = Purchases
+        fields = (
+            'id',
+            'user',
+            'user_id',
+            'brand',
+            'brand_id',
+            'product', 
+            'product_id', 
+            'quantity',
+            'price',
+            'price_total'
+        )
+
+
+    
--- 4_Django Projects/06_Stock_App_Project/stock/views.py
+++ 4_Django Projects/06_Stock_App_Project/stock/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Brand, Category, Firm, Product, Purchases, Sales
+from .serializer import CategorySerializer, BrandSerializer, ProductSerializer, FirmSerializer, PurchasesSerializer, SalesSerializer
 
-# Create your views here.
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class FirmView(viewsets.ModelViewSet):
+    queryset = Firm.objects.all()
+    serializer_class = FirmSerializer
+
+class PurchasesView(viewsets.ModelViewSet):
+    queryset = Purchases.objects.all()
+    serializer_class = PurchasesSerializer
+
+class SalesView(viewsets.ModelViewSet):
+    queryset = Sales.objects.all()
+    serializer_class = SalesSerializer
