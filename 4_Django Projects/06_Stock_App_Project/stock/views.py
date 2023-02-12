f817970c9063d191b75ce2bed92b86aa33da4e30
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-12 21:01:45 +0300

    CategoryProductSerializer_serializers-views(v.3)

--- 4_Django Projects/06_Stock_App_Project/stock/views.py
+++ 4_Django Projects/06_Stock_App_Project/stock/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets,filters
+from rest_framework.permissions import DjangoModelPermissions
 from .models import Brand, Category, Firm, Product, Purchases, Sales
-from .serializer import CategorySerializer, BrandSerializer, ProductSerializer, FirmSerializer, PurchasesSerializer, SalesSerializer
+from .serializer import CategorySerializer, BrandSerializer, ProductSerializer, FirmSerializer, PurchasesSerializer, SalesSerializer,CategoryProductSerializer
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = [filters.SearchFilter]
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter,DjangoFilterBackend]
     search_fields = ['name']
+    filterset_fields = ['name']
+    
+    def get_serializer_class(self):
+        if self.request.query_params.get('name'):
+            return CategoryProductSerializer
+        return super().get_serializer_class()
+
 
 class BrandView(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
