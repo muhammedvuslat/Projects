0d50e0e386e3fbf996e294e0a7a7541e0074e5b0
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-14 00:20:11 +0300

    Sales_CRUD_methods(v.7.1)

--- 4_Django Projects/06_Stock_App_Project/stock/views.py
+++ 4_Django Projects/06_Stock_App_Project/stock/views.py
@@ -92,10 +92,6 @@ class PurchasesView(viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    
-
-
-    
 
 class SalesView(viewsets.ModelViewSet):
     queryset = Sales.objects.all()
@@ -103,3 +99,60 @@ class SalesView(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter,DjangoFilterBackend]
     search_fields = ['product']
     filterset_fields = ['product']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        #!||||||<Sales oluşturulması>||||||
+        sales = request.data
+        product = Product.objects.get(id=sales["product_id"])
+        if sales["quantity"] <= product.stock:
+            product.stock -= sales["quantity"]
+            product.save()
+        else:
+            data = {
+                "message": f"Dont have enough stock, current stock is {product.stock}"
+            }
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+        #!||||||<Sales oluşturulması>||||||
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        #!||||||<Sales güncellenmesi>||||||
+        sale = request.data
+        product= Product.objects.get(id=instance.product_id)          
+        if sale["quantity"] > instance.quantity: 
+
+            if sale["quantity"] <= instance.quantity + product.stock: 
+                product.stock = instance.quantity + product.stock - sale["quantity"]
+                product.save()
+            else:
+                data = {
+                "message": f"Dont have enough stock, current stock is {product.stock}"
+                }
+                return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+        elif sale["quantity"] <= instance.quantity:
+            product.stock += instance.quantity - sale["quantity"]
+            product.save()
+        #!||||||<Sales güncellenmesi>||||||
+        self.perform_update(serializer)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        #!||||||<Sales silinmesi>||||||
+        product = Product.objects.get(id=instance.product_id)
+        product.stock += instance.quantity
+        product.save()
+        #!||||||<Sales silinmesi>||||||
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
