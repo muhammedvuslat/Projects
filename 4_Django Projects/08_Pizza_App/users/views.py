4a957fb6fa051d6bf4a04d4b8338fde39189a87e
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-20 10:19:57 +0300

    views.py-forms.py(v.5)

--- 4_Django Projects/08_Pizza_App/users/views.py
+++ 4_Django Projects/08_Pizza_App/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
 
 def register(request):
-    form = UserCreationForm()
+    form =UserForm()
+
     if request.method == 'POST':
-        form =UserCreationForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
