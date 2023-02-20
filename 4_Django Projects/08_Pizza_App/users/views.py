047ad092c6e4c20c0a667a59834294bf754c0c25
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-20 11:41:13 +0300

    login-views(v.6)

--- 4_Django Projects/08_Pizza_App/users/views.py
+++ 4_Django Projects/08_Pizza_App/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UserForm
+from .forms import UserForm, LoginForm
+from django.contrib.auth import login
 
 def register(request):
     form =UserForm()
@@ -16,3 +17,19 @@ def register(request):
     }
 
     return render(request, 'users/register.html', context)
+
+def user_login(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('home')
+
+    context = {
+        'form':form
+    }
+
+    return render(request,'users/login.html',context)
