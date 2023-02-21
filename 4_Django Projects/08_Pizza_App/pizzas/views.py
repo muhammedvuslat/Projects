96245d5d751f4f7704aa14059b8dcb62c97f137a
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-21 23:46:52 +0300

    pizzas_styls(v.9)

--- 4_Django Projects/08_Pizza_App/pizzas/views.py
+++ 4_Django Projects/08_Pizza_App/pizzas/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Pizza
 
 def home(request):
     return render(request,'pizzas/home.html')
 
 def pizzas(request):
-    return render(request,'pizzas/pizzas.html')
+    pizzas = Pizza.objects.all()
+
+    context = {
+        'pizzas':pizzas
+    }
+
+    return render(request,'pizzas/pizzas.html',context)
