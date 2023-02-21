66e2c090af04db57bac4cb61d8d3090d96e5fe99
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-21 17:54:19 +0300

    pizzas_models_add-urls_settingsv.8)

--- 4_Django Projects/08_Pizza_App/pizzas/models.py
+++ 4_Django Projects/08_Pizza_App/pizzas/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    topping = models.ManyToManyField(Topping)
+    image = models.ImageField(upload_to= 'pizza_pics', null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+
+Size = (
+    ('s' , 'Small'),
+    ('m' , 'Medium'),
+    ('l' , 'Large')
+)
+
+class Order(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(choices=Size, default='m',max_length=1)
+    quantiy = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.pizza.name} order {self.user.username}'
--- 4_Django Projects/08_Pizza_App/pizzas/views.py
+++ 4_Django Projects/08_Pizza_App/pizzas/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'pizzas/home.html')
+
+def pizzas(request):
+    return render(request,'pizzas/pizzas.html')
