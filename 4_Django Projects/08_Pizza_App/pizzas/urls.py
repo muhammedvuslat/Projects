2c1d5ff5c915ce32ff1a69d6ff81a014df0aed76
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-22 23:41:14 +0300

    pizza_order(v.10)

--- 4_Django Projects/08_Pizza_App/pizzas/urls.py
+++ 4_Django Projects/08_Pizza_App/pizzas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home,pizzas
+from .views import home,pizzas,order_view
 
 urlpatterns = [
     path("",home, name="home"),
-    path("pizzas/",pizzas, name="pizzas")
+    path("pizzas/",pizzas, name="pizzas"),
+    path("pizzas<int:id>/",order_view, name="order")
 ]
