df4a6c833dc7712d5004a537d99bbf94ae17b1c6
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-23 09:06:22 +0300

    order_update_page(v.12)

--- 4_Django Projects/08_Pizza_App/pizzas/urls.py
+++ 4_Django Projects/08_Pizza_App/pizzas/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,pizzas,order_view, my_orders
+from .views import home,pizzas,order_view, my_orders, order_update
 
 urlpatterns = [
     path("",home, name="home"),
     path("pizzas/",pizzas, name="pizzas"),
     path("pizzas<int:id>/",order_view, name="order"),
-    path("my_orders/",my_orders, name="my_orders")
+    path("my_orders/",my_orders, name="my_orders"),
+    path("order_update/<int:id>",order_update, name="order_update")
 ]
