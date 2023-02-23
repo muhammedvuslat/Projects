e17169d9f7012626391e710aaccb96428b543abc
muhammedvuslat <v.sltcevikev@gmail.com>
2023-02-23 09:10:11 +0300

    order_delete(v.13)

--- 4_Django Projects/08_Pizza_App/pizzas/urls.py
+++ 4_Django Projects/08_Pizza_App/pizzas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home,pizzas,order_view, my_orders, order_update
+from .views import home,pizzas,order_view, my_orders, order_update,order_delete
 
 urlpatterns = [
     path("",home, name="home"),
     path("pizzas/",pizzas, name="pizzas"),
     path("pizzas<int:id>/",order_view, name="order"),
     path("my_orders/",my_orders, name="my_orders"),
-    path("order_update/<int:id>",order_update, name="order_update")
+    path("order_update/<int:id>",order_update, name="order_update"),
+    path("order_delete/<int:id>",order_delete, name="order_delete")
 ]
