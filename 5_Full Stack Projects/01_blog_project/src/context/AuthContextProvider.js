2f26c2da4dda2a5cb0d8849b56f294b4654cffb8
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-02 21:23:52 +0300

    Auth_provider(v.4.2)

--- 5_Full Stack Projects/01_blog_project/src/context/AuthContextProvider.js
+++ 5_Full Stack Projects/01_blog_project/src/context/AuthContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 //! Authentiocation verileri için context oluşturma
 const AuthContext = createContext();
@@ -9,8 +9,53 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
-const AuthContextProvider = () => {
-  return <div>AuthContextProvider</div>;
-};
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-export default AuthContextProvider;
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    signOut(auth);
+  }
+
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account" });
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    loginWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
