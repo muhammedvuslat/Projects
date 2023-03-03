6cc7c48fa0e3812b6c1510315669d827325c27a7
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-04 00:02:32 +0300

    Temperature_control_button

--- 3_Mini Projects/02_React_Temperature_Project/temperature_control/src/App.js
+++ 3_Mini Projects/02_React_Temperature_Project/temperature_control/src/App.js
@@ -1,7 +1,51 @@
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+import "./App.css";
+import "./style.css";
+
+const App = () => {
+  const [tempValue, setTempValue] = useState(20);
+  const [tempColor, setTempColor] = useState("mid");
+
+  const increaseTemp = () => {
+    if (tempValue === 40) return;
+
+    const newTemp = tempValue + 1;
+
+    setTempValue(newTemp);
+    if (tempValue > 20) {
+      setTempColor("hot");
+    } else if ((tempValue = 20)) {
+      setTempColor("mid");
+    }
+  };
+
+  const decreaseTemp = () => {
+    if (tempValue === 0) return;
+
+    const newTemp = tempValue - 1;
+
+    setTempValue(newTemp);
+    if (tempValue < 20) {
+      setTempColor("cold");
+    } else if ((tempValue = 20)) {
+      setTempColor("mid");
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display  ${tempColor} `}>
+          {tempValue}°C
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={increaseTemp}>+</button>
+        <button onClick={decreaseTemp}>-</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
