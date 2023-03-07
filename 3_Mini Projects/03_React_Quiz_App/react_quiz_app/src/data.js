671e64510e76d197d3634756bd9cbac377474f73
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-07 14:02:21 +0300

    Quiz_app

---
+++ 3_Mini Projects/03_React_Quiz_App/react_quiz_app/src/data.js
@@ -0,0 +1,114 @@
+const Questions = [
+  {
+    questionText: "Türkiye'nin başkenti neresidir?",
+    answerOptions: [
+      { answerText: "İstanbul", isCorrect: false },
+      { answerText: "Ankara", isCorrect: true },
+      { answerText: "İzmir", isCorrect: false },
+      { answerText: "Bursa", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as2.ftcdn.net/v2/jpg/02/93/56/33/1000_F_293563333_StGy2aQnqZ7ZYZgxcxTI63ZrWcyRCxEV.jpg",
+  },
+  {
+    questionText: "Atatürk hangi yıl doğmuştur?",
+    answerOptions: [
+      { answerText: "1881", isCorrect: true },
+      { answerText: "1889", isCorrect: false },
+      { answerText: "1898", isCorrect: false },
+      { answerText: "1905", isCorrect: false },
+    ],
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4a/Mustafa_Kemal_Atat%C3%BCrk.jpg",
+  },
+  {
+    questionText: "Dünya'nın en yüksek dağı hangisidir?",
+    answerOptions: [
+      { answerText: "K2", isCorrect: false },
+      { answerText: "Mont Blanc", isCorrect: false },
+      { answerText: "Everest", isCorrect: true },
+      { answerText: "Aconcagua", isCorrect: false },
+    ],
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2022/05/16/01/15/road-7199274_960_720.jpg",
+  },
+  {
+    questionText: "Bir yılda kaç mevsim vardır?",
+    answerOptions: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "2", isCorrect: false },
+      { answerText: "3", isCorrect: false },
+      { answerText: "4", isCorrect: true },
+    ],
+    imageUrl:
+      "https://as1.ftcdn.net/v2/jpg/01/07/12/04/1000_F_107120494_7Z5PeYLan009FRB7yrhvfqako4mzyW7v.jpg",
+  },
+  {
+    questionText: "Bir olimpiyat kaç yılda bir düzenlenir?",
+    answerOptions: [
+      { answerText: "2", isCorrect: false },
+      { answerText: "4", isCorrect: true },
+      { answerText: "6", isCorrect: false },
+      { answerText: "8", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as2.ftcdn.net/v2/jpg/01/14/13/69/1000_F_114136983_jUTScoeTJyXR6md5T3c4sINbuLT67D0g.jpg",
+  },
+  {
+    questionText: "Mona Lisa hangi İtalyan ressamın eseridir?",
+    answerOptions: [
+      { answerText: "Leonardo da Vinci", isCorrect: true },
+      { answerText: "Michelangelo", isCorrect: false },
+      { answerText: "Raffaello", isCorrect: false },
+      { answerText: "Caravaggio", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as1.ftcdn.net/v2/jpg/03/71/94/32/1000_F_371943237_EgnRK45LLKsrq2d4CNcbNPSSTmP9xdW1.jpg",
+  },
+  {
+    questionText: "Dünya nüfusunun yaklaşık olarak kaçı Asya'da yaşamaktadır?",
+    answerOptions: [
+      { answerText: "20%", isCorrect: false },
+      { answerText: "40%", isCorrect: false },
+      { answerText: "60%", isCorrect: true },
+      { answerText: "80%", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as2.ftcdn.net/v2/jpg/00/35/45/31/1000_F_35453192_LAp0FOdFManLNtUKSmzQ9IhPWt40Prca.jpg",
+  },
+  {
+    questionText: "İlk yazılı Türk destanı hangisidir?",
+    answerOptions: [
+      { answerText: "Oğuz Kağan Destanı", isCorrect: true },
+      { answerText: "Alp Er Tunga Destanı", isCorrect: false },
+      { answerText: "Bozkurtlar Diriliyor Destanı", isCorrect: false },
+      { answerText: "Dede Korkut Hikayeleri", isCorrect: false },
+    ],
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c2/TM-2011-20manat-Oguz_Han2-b.png",
+  },
+  {
+    questionText: "Atatürk Hangi Tarihte Ölmüştür?",
+    answerOptions: [
+      { answerText: "19 Mayıs 1919", isCorrect: false },
+      { answerText: "10 Kasım 1938", isCorrect: true },
+      { answerText: "23 Nisan 1920", isCorrect: false },
+      { answerText: "29 Ekim 1923", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as2.ftcdn.net/v2/jpg/02/30/77/75/1000_F_230777587_wcXSiPLvno9TQxEbzjepZBIQwEiTpcmc.jpg",
+  },
+  {
+    questionText: "Türkiye'nin En Büyük Gölü Hangisidir?",
+    answerOptions: [
+      { answerText: "Beyşehir Gölü", isCorrect: false },
+      { answerText: "Van Gölü", isCorrect: true },
+      { answerText: "Eğirdir Gölü", isCorrect: false },
+      { answerText: "Tuz Gölü", isCorrect: false },
+    ],
+    imageUrl:
+      "https://as1.ftcdn.net/v2/jpg/00/68/57/70/1000_F_68577093_uAgnrunoxziD9aRBus79gzDuQnzD9Bkt.jpg",
+  },
+];
+
+export default Questions;
