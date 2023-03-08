4a2c1124115f26d05deb0469deb7219b35decf75
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-08 23:53:42 +0300

    views_start-bs4-request(v.3)

--- 5_Full Stack Projects/02_news_project/news/views.py
+++ 5_Full Stack Projects/02_news_project/news/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from bs4 import BeautifulSoup as BSoup
+from news.models import News
 
-# Create your views here.
+def scape(request):
+    session = request.Sessions()
+    session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+    url = "xxxxx"
+
+    content = session.get(url, verify=False).content
+    soup = BSoup(content, "html.parser")
+    News = soup.find_all('div', {"class":"curation-module__item"})
+
+
+    return redirect('../')
