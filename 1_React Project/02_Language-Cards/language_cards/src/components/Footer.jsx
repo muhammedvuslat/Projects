0034bb8a3eab11d4779b4d341a6a46d957fd6191
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-14 12:01:09 +0300

    Footer(v.5)

---
+++ 1_React Project/02_Language-Cards/language_cards/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-light">
+        <div className="rounded-4 mt-2 bg-secondary text-center py-1">
+          <p className=" mb-0 text-light">© 2023 by Vuslat</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
