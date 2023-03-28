6b5985a7cbb46fa73cc26ad39f031aa969c6de71
muhammedvuslat <v.sltcevikev@gmail.com>
2023-03-28 22:04:17 +0300

    register&login-page

--- 5_Full Stack Projects/03_e_commerce/front-end/src/router/AppRouter.jsx
+++ 5_Full Stack Projects/03_e_commerce/front-end/src/router/AppRouter.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const Approuter = () => {
-  return <div>Approuter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Approuter;
