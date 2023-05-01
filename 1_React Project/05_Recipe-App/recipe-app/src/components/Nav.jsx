580b7f584695a088f06f47631a3145cd1c9b4509
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-01 22:13:43 +0300

    Navbar

--- 1_React Project/05_Recipe-App/recipe-app/src/components/Nav.jsx
+++ 1_React Project/05_Recipe-App/recipe-app/src/components/Nav.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BrandStyle, NavStyle } from "../styles/components.style/Nav.style";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <h1>Nav Components</h1>
-    </div>
+    <NavStyle>
+      <BrandStyle>
+        <i>{"Vuslat/"}</i>
+        <span>Recipe</span>
+      </BrandStyle>
+      <div
+        className="Hamburger"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <GiHamburgerMenu />
+      </div>
+      <div className="MenuStyled">
+        <p to="/">Home</p>
+        <p to="about">About</p>
+        <p to="register">Register</p>
+        <p to="/">LOG</p>
+      </div>
+    </NavStyle>
   );
 };
 
--- 1_React Project/05_Recipe-App/recipe-app/src/styles/components.style/Nav.style.jsx
+++ 1_React Project/05_Recipe-App/recipe-app/src/styles/components.style/Nav.style.jsx
@@ -1,9 +1,23 @@
-const Nav = () => {
-  return (
-    <div>
-      <h1>Nav Components</h1>
-    </div>
-  );
-};
+import styled from "styled-components";
+import Flex from "../global.style/Flex.style";
+import { Link } from "react-router-dom";
 
-export default Nav;
+export const NavStyle = styled(Flex)`
+  /* NavStyle */
+`;
+
+export const BrandStyle = styled(Link)`
+  /* BrandStyle */
+`;
+
+export const MenuStyled = styled(Flex)`
+  /* MenuStyled */
+`;
+
+export const MenuLink = styled(Link)`
+  /* MenuLink p tag */
+`;
+
+export const Hamburger = styled.div`
+  /* Hamburger div */
+`;
