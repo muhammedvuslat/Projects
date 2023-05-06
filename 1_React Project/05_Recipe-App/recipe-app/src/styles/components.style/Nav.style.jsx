0dc4874f550a1a3b0587f36649bdb580f39e2eca
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-06 06:54:46 +0300

    Navbar(v.3)

--- 1_React Project/05_Recipe-App/recipe-app/src/styles/components.style/Nav.style.jsx
+++ 1_React Project/05_Recipe-App/recipe-app/src/styles/components.style/Nav.style.jsx
@@ -22,12 +22,39 @@ export const BrandStyle = styled(Link)`
 
 export const MenuStyled = styled(Flex)`
   flex-direction: column;
+  border: 4px solid yellow;
+  width: 100%;
+  overflow: hidden;
 `;
 
 export const MenuLink = styled(Link)`
   /* MenuLink p tag */
+  border: 2px solid brown;
+  text-align: center;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  /*  color theme dan alınacak */
+  color: brown;
+  text-decoration: none;
+  transition: all 0.3s ease-in;
+  &:hover {
+    //todo
+    /* hover color theme dan alınacak */
+    color: pink;
+    font-weight: bold;
+  }
+  //todo
+  /* media ayarları theme den alınacak */
+  /* @media */
 `;
 
 export const Hamburger = styled.div`
   /* Hamburger div */
+  border: 4px solid blue;
+  display: none;
+  cursor: pointer;
+  color: red;
+  //todo
+  /* media ayarları theme den alınacak */
+  /* @media */
 `;
