753043e7cc12bd855d509955f0528183e9ba1186
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-07 15:42:42 +0300

    Home_img(v.8.1)

--- 1_React Project/05_Recipe-App/recipe-app/src/styles/pages.style/Home.style.jsx
+++ 1_React Project/05_Recipe-App/recipe-app/src/styles/pages.style/Home.style.jsx
@@ -1,19 +1,6 @@
 import styled from "styled-components";
 import backGroud from "../../assets/homeBG.svg";
 
-// export const ImgDiv = styled.div`
-//   /* İmg divi */
-//   display: flex;
-//   justify-content: center;
-//   margin: 2rem;
-// `;
-
-// export const HomeImg = styled.img`
-//   /* Home img */
-//   width: 80%;
-//   max-width: 750px;
-// `;
-
 export const HeaderText = styled.h1`
   /* Home Text */
   font-size: 2.5rem;
@@ -22,9 +9,12 @@ export const HeaderText = styled.h1`
 `;
 
 export const HomeDiv = styled.div`
-  /* border: 2px solid red; */
+  border: 2px solid red;
   background-image: url(${backGroud});
   background-size: cover;
   background-position: center center;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: ${({ recipes }) => (recipes === null ? "center" : "")};
 `;
