8bca10ea8c0dc88fc50e4fd13d9a447817275b43
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-11 20:15:36 +0300

    Back-end(v.9)

--- 5_Full Stack Projects/03_e_commerce/back-end/ecommerce/serializers.py
+++ 5_Full Stack Projects/03_e_commerce/back-end/ecommerce/serializers.py
@@ -46,10 +46,10 @@ class OrderItemSerializer(serializers.ModelSerializer):
     def get_item_total_price(self, obj):
         return obj.quantity * obj.item.price
     
-    def create(self, validated_data):
-        validated_data["user_id"] = self.context["request"].user.id
-        instance = OrderItem.objects.create(**validated_data)
-        return instance
+    # def create(self, validated_data):
+    #     validated_data["user_id"] = self.context["request"].user.id
+    #     instance = OrderItem.objects.create(**validated_data)
+    #     return instance
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
@@ -62,46 +62,77 @@ class AddressSerializer(serializers.ModelSerializer):
             "country",
         )
 
-class OrderListSerializer(serializers.ModelSerializer):
+# class OrderListSerializer(serializers.ModelSerializer):
 
-    items = OrderItemSerializer(many=True, required=True)
-    order_total_price = serializers.SerializerMethodField()
-    address = AddressSerializer(read_only=True)
-    address_id = serializers.IntegerField()
+#     items = OrderItemSerializer(many=True, required=True)
+#     order_total_price = serializers.SerializerMethodField()
+#     address = AddressSerializer(read_only=True)
+#     address_id = serializers.IntegerField()
 
-    class Meta:
-        model = Order
-        fields = (
-            "id",
-            "user",
-            "items",
-            "start_date",
-            "shipping_date",
-            "ordered",
-            "address_id",
-            "address", 
-            "payment",
-            "order_total_price"
-        )
+#     class Meta:
+#         model = Order
+#         fields = (
+#             "id",
+#             "user",
+#             "items",
+#             "start_date",
+#             "shipping_date",
+#             "ordered",
+#             "address_id",
+#             "address", 
+#             "payment",
+#             "order_total_price"
+#         )
     
-    def get_order_total_price(self, obj):
-        items = obj.items.all()
+#     def get_order_total_price(self, obj):
+#         items = obj.items.all()
 
-        total = 0
+#         total = 0
 
-        for i in items:
-            total += i.quantity * i.item.price
-        return total
+#         for i in items:
+#             total += i.quantity * i.item.price
+#         return total
 
+# class OrderSerializer(serializers.ModelSerializer):
+
+#     order_total_price = serializers.SerializerMethodField()
+#     address = AddressSerializer(read_only=True)
+#     address_id = serializers.IntegerField()
+#     user = serializers.StringRelatedField()
+#     user_id = serializers.IntegerField()
+#     class Meta:
+#         model = Order
+#         fields = (
+#             "id",
+#             "user_id",
+#             "user",
+#             "items",
+#             "start_date",
+#             "shipping_date",
+#             "ordered",
+#             "address_id",
+#             "address",  
+#             "payment",
+#             "order_total_price"
+#         )
+    
+#     def get_order_total_price(self, obj):
+#         items = obj.items.all()
+
+#         total = 0
+
+#         for i in items:
+#             total += i.quantity * i.item.price
+#         return total
+    
 class OrderSerializer(serializers.ModelSerializer):
-
+    
+    items = OrderItemSerializer(many=True, read_only=True)
     order_total_price = serializers.SerializerMethodField()
     address = AddressSerializer(read_only=True)
-    # address = serializers.StringRelatedField()
     address_id = serializers.IntegerField()
     user = serializers.StringRelatedField()
-    user_id = serializers.IntegerField()
-
+    user_id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Order
         fields = (
@@ -126,3 +157,19 @@ class OrderSerializer(serializers.ModelSerializer):
         for i in items:
             total += i.quantity * i.item.price
         return total
+    
+    # def create(self, validated_data):
+    #     # print(validated_data, "eeeeeeeeeeee")
+    #     # items_data = validated_data.pop("items_data") 
+    #     order = Order.objects.create(user=self.context['request'].user)
+    #     order.items.set(validated_data["items"])
+    #     order.save()
+    #     return order
+    
+    def create(self, validated_data):
+        items = self.context['request'].data.get('items', [])
+        order = Order.objects.create(user=self.context['request'].user)
+        order.items.set(items)
+        order.save()
+
+        return order
--- 5_Full Stack Projects/03_e_commerce/back-end/ecommerce/views.py
+++ 5_Full Stack Projects/03_e_commerce/back-end/ecommerce/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import (
@@ -13,11 +13,12 @@ from .models import (
 from .serializers import (
     ItemSerializer,
     OrderItemSerializer,
-    OrderListSerializer,
+    # OrderListSerializer,
     OrderSerializer,
     AddressSerializer,
     )
 from .permissions import IsStaffOrReadOnly
+from rest_framework.filters import SearchFilter
 
 
 class ItemMVS(ModelViewSet):
@@ -25,6 +26,9 @@ class ItemMVS(ModelViewSet):
     serializer_class = ItemSerializer
     permission_classes = [IsStaffOrReadOnly,]
     lookup_field = "slug"
+    filter_backends = [SearchFilter]
+    # filterset_fields=['title']
+    search_fields=['description']
 
 
 class OrderItemMVS(ModelViewSet):
@@ -38,13 +42,13 @@ class OrderItemMVS(ModelViewSet):
         if user.is_staff:
             return OrderItem.objects.all()
         
-        return OrderItem.objects.filter(user=user)
+        return OrderItem.objects.filter(user=user, ordered=False)
     
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        orderItem_queryset = OrderItem.objects.filter(user_id=request.user.id, item_id=request.data["item_id"])
+        orderItem_queryset = OrderItem.objects.filter(user_id=request.user.id, item_id=request.data["item_id"], ordered=False)
         
         if orderItem_queryset.exists():
             data ={
@@ -77,44 +81,73 @@ class OrderItemMVS(ModelViewSet):
 
         return Response(serializer.data)
 
-class OrderListView(ListAPIView):
+# class OrderListView(ListAPIView):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderListSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         user = self.request.user
+
+#         if user.is_staff:
+#             return Order.objects.all()
+        
+#         return Order.objects.filter(user=user)
+    
+# class OrderCreateView(CreateAPIView):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         user = self.request.user
+
+#         if user.is_staff:
+#             return Order.objects.all()
+        
+#         return Order.objects.filter(user=user)
+
+# class OrderDetailView(RetrieveUpdateDestroyAPIView):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         user = self.request.user
+
+#         if user.is_staff:
+#             return Order.objects.all()
+        
+#         return Order.objects.filter(user=user)
+    
+class OrderView(ModelViewSet):
     queryset = Order.objects.all()
-    serializer_class = OrderListSerializer
+    serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
 
-        # if user.is_staff:
-        #     return Order.objects.all()
+        if user.is_staff:
+            return Order.objects.all()
         
         return Order.objects.filter(user=user)
     
-class OrderCreateView(CreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-
-        if user.is_staff:
-            return Order.objects.all()
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         
-        return Order.objects.filter(user=user)
+        for i in request.data["items"]:
+            orderItem = OrderItem.objects.get(id=i)
+            orderItem.ordered = True
+            orderItem.save()
+            
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-class OrderDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-
-        if user.is_staff:
-            return Order.objects.all()
-        
-        return Order.objects.filter(user=user)
+    def perform_create(self, serializer):
+        serializer.save()
 
 class AddressMVS(ModelViewSet):
     queryset = Address.objects.all()
@@ -123,4 +156,8 @@ class AddressMVS(ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+
+        if user.is_staff:
+            return Address.objects.all()
+        
         return Address.objects.filter(user=user)
