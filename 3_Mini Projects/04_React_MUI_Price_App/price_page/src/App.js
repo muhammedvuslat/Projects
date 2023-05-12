69b426417d1adbc8f0f9f7b69b17bea65a9f822a
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-12 09:45:33 +0300

    Page_start_setups(v.0.1)

---
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/App.js
@@ -0,0 +1,19 @@
+import Cards from "./components/Cards";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Cards />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
