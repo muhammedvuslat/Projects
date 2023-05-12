f6bb47d68079d0a56cba3195fcf8c823e6bf6c81
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-12 10:25:05 +0300

    Page_footer(v.1)

--- 3_Mini Projects/04_React_MUI_Price_App/price_page/src/App.js
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/App.js
@@ -1,17 +1,31 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import Cards from "./components/Cards";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 
+const themeSelect = createTheme({
+  palette: {
+    primary: {
+      main: "#0d47a1",
+    },
+    secondary: {
+      main: "#03a9f4",
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <Nav />
-      <Header />
-      <Cards />
-      <Content />
-      <Footer />
+      <ThemeProvider theme={themeSelect}>
+        <Nav />
+        <Header />
+        <Cards />
+        <Content />
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
--- 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Footer.jsx
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Footer.jsx
@@ -1,7 +1,27 @@
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <Typography
+        variant="body2"
+        color="secondary"
+        align="center"
+        sx={{ mt: 5 }}
+      >
+        {"Copyright © "}
+        <Link
+          rel="stylesheet"
+          href="https://github.com/muhammedvuslat"
+          color="inherit"
+        >
+          My Github
+        </Link>
+      </Typography>
+    </>
+  );
 };
 
 export default Footer;
