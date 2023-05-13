7b6fa7c98f784e246a1f7d4467d169788f24c815
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-13 07:53:04 +0300

    Page_nav(v.2)

--- 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Nav.jsx
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Nav.jsx
@@ -1,8 +1,60 @@
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import Link from "@mui/material/Link";
+
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <>
+      <AppBar
+        position="static"
+        color="primary"
+        elevation={0}
+        sx={{
+          borderBottom: (theme) => `2px solid ${theme.palette.primary.dark}`,
+        }}
+      >
+        <Toolbar sx={{ flexWrap: "wrap" }}>
+          <Typography color="inherit" variant="h6" noWrap sx={{ flexGrow: 1 }}>
+            Pure Payment
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="secondary.dark"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+              underline="none"
+            >
+              Features
+            </Link>
+            <Link
+              variant="button"
+              color="secondary.dark"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+              underline="none"
+            >
+              Support
+            </Link>
+            <Link
+              variant="button"
+              color="secondary.dark"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+              underline="none"
+            >
+              Enterprise
+            </Link>
+            <Button
+              sx={{ my: 1, mx: 1.5 }}
+              variant="outlined"
+              color="secondary"
+            >
+              Login
+            </Button>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
