eb3bd30bf8b4b8aeab4d525da38b5b5b392ac65a
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-13 08:14:23 +0300

    Page_Header-Content(v.3)

--- 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Content.jsx
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Content.jsx
@@ -1,7 +1,24 @@
+import { Grid } from "@mui/material";
+import Container from "@mui/material/Container";
 import React from "react";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <>
+      <Container
+        maxWidth="md"
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.primary.dark}`,
+          borderBottom: (theme) => `1px solid ${theme.palette.primary.dark}`,
+          mt: 8,
+          py: [3, 6],
+        }}
+      >
+        <Grid></Grid>
+      </Container>
+    </>
+  );
 };
 
 export default Content;
--- 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Header.jsx
+++ 3_Mini Projects/04_React_MUI_Price_App/price_page/src/components/Header.jsx
@@ -1,7 +1,33 @@
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Container>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="primary.dark"
+          gutterBottom
+        >
+          Pricing
+        </Typography>
+        <Typography
+          component="p"
+          variant="h5"
+          align="center"
+          color="secondary.dark"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+          adipisci fugiat accusantium aliquam impedit autem, ratione nisi quidem
+          et a.
+        </Typography>
+      </Container>
+    </>
+  );
 };
 
 export default Header;
