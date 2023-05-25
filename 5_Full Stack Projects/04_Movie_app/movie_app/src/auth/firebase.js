56730cfecaa7f30601f3d4738ad5787710cb908d
muhammedvuslat <v.sltcevikev@gmail.com>
2023-05-25 12:06:17 +0300

    fire_base_start(v4.0)

--- 5_Full Stack Projects/04_Movie_app/movie_app/src/auth/firebase.js
+++ 5_Full Stack Projects/04_Movie_app/movie_app/src/auth/firebase.js
@@ -1,7 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 //* Your web app's Firebase configuration
+//* Your web app's Firebase configuration
+// TODO: Replace the following with your app's Firebase project configuration
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
+//! Normal kullanımda google firebase de config ayarları bu alana yapıştırılır fakat veri gizliliği ve korunması adına .env dosyasına ekleyip bu konumdan .env dan alınmasını sağlıyoruz
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
@@ -15,3 +21,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = async (email, password) => {
+  try {
+    let userTest = await createUserWithEmailAndPassword(auth, email, password);
+    console.log(userTest);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
