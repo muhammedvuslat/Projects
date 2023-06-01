d07c5a118ceffe5615c87dbb6e767e5a9f0970c2
muhammedvuslat <v.sltcevikev@gmail.com>
2023-06-01 20:01:49 +0300

    auth_user(v6.1)

--- 5_Full Stack Projects/04_Movie_app/movie_app/src/App.js
+++ 5_Full Stack Projects/04_Movie_app/movie_app/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
       <AuthContextProvider>
         <AppRouter />
       </AuthContextProvider>
+      {/* App Router AutContectProvider ile sarmalladığından dolayı children propunun özelliği olarark içeriğinde bulunan bütün yapı iletilecektir. ayrı olarak <AuthContextProvider / children={<AppRouter/>}> şeklinde bildirilmesine gerek yoktur //! Not prop gönderilme durumlarında zaten self closing gereklidir  */}
     </div>
   );
 };
--- 5_Full Stack Projects/04_Movie_app/movie_app/src/context/AuthContextProvider.jsx
+++ 5_Full Stack Projects/04_Movie_app/movie_app/src/context/AuthContextProvider.jsx
@@ -3,15 +3,22 @@ import { userState } from "../auth/firebase";
 
 export const AuthContext = createContext();
 
-const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(true);
+//! useContext costum hook alternatif tip 2
+// export const useAuthContext = ( ) => {
+//   return useConntext(AuthContext)
+// }
+
+const AuthContextProvider = (props) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  console.log(currentUser);
+
   useEffect(() => {
-    userState();
+    userState(setCurrentUser);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser }}>
-      {children}
+    <AuthContext.Provider value={currentUser}>
+      {props.children}
     </AuthContext.Provider>
   );
 };
